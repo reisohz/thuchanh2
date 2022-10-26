653e7a0d5ba72852c7fcb5bc43189251593d6245
khoinguyen <nhoknguyencutehj@gmail.com>
2022-10-26 10:03:47 +0700

    bai thuc hanh 2

---
+++ baithuchanh2.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+void nhapmang(int a[10], int n);
+void xuatmang(int a[10], int n);
+void giatriln(int a[10], int n);
+void tinhtich(int a[10], int n);
+int main()
+{
+int a[10], n;
+printf("nhap so ptu cua mang n=");
+scanf("%d",&n);
+nhapmang(a,n);
+xuatmang(a,n);
+giatriln(a,n);
+return 0; 
+}
+
+void nhapmang(int a[10], int n)
+{
+	for(int i=0;i<n;i++)
+
+   {
+
+     printf("nhap so ptu cua mang la a[%d]= ",i);
+     scanf("%d",&a[i]);
+     
+   }
+}
+
+void xuatmang(int a[10], int n)
+{
+  	printf("\n cac ptu cua mang n=");
+  	  for(int j=0;j<n;j++)
+  	  printf("%d   ;    ",a[j]);
+}
+void giatriln(int a[10], int n)
+{int max=0 ,i;
+for (int i=0;i<n;i++)
+if (a[i]>max)
+max = a[i];
+printf("\n So phan tu lon nhat la la %d", max);
+
+}
+
+
